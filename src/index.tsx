b3b24eacd6bc088e9535b0a798823ab80b6db4bc
Xinyan Chen <xinychen@microsoft.com>
2023-08-01 15:07:07 +0800

    Update usage.

--- src/index.tsx
+++ src/index.tsx
@@ -1,54 +1,51 @@
 import {
-    Tree,
-    TreeItem,
-    TreeItemLayout
+  Tree,
+  TreeItem,
+  TreeItemLayout
 } from "@fluentui/react-components/unstable";
-  
-  export const DefaultOpenTrees = () => {
-    const defaultOpenTrees = [
-      "default-subtree-1",
-      "default-subtree-2",
-      "default-subtree-2-1"
-    ];
-  
-    return (
-      <Tree aria-label="Tree" defaultOpenItems={defaultOpenTrees}>
-        <TreeItem value="default-subtree-1">
-          <TreeItemLayout>level 1, item 1</TreeItemLayout>
-          <Tree>
-            <TreeItem content={{ className: "test-class" }}>
-              <div>
-                <TreeItemLayout>level 2, item 1</TreeItemLayout>
-              </div>
-            </TreeItem>
-            <TreeItem>
-              <TreeItemLayout>level 2, item 2</TreeItemLayout>
-            </TreeItem>
-            <TreeItem>
-              <TreeItemLayout>level 2, item 3</TreeItemLayout>
-            </TreeItem>
-          </Tree>
-        </TreeItem>
-        <TreeItem value="default-subtree-2">
-          <TreeItemLayout>level 1, item 2</TreeItemLayout>
-          <Tree>
-            <TreeItem value="default-subtree-2-1">
-              <TreeItemLayout>level 2, item 1</TreeItemLayout>
-              <Tree>
-                <TreeItem>
-                  <TreeItemLayout>level 3, item 1</TreeItemLayout>
-                </TreeItem>
-                <TreeItem>
-                  <TreeItemLayout>level 3, item 2</TreeItemLayout>
-                </TreeItem>
-                <TreeItem>
-                  <TreeItemLayout>level 3, item 3</TreeItemLayout>
-                </TreeItem>
-              </Tree>
-            </TreeItem>
-          </Tree>
-        </TreeItem>
-      </Tree>
-    );
-  };
-  
+
+export const DefaultOpenTrees = () => {
+  const defaultOpenTrees = [
+    "default-subtree-1",
+    "default-subtree-2",
+    "default-subtree-2-1"
+  ];
+
+  return (
+    <Tree aria-label="Tree" defaultOpenItems={defaultOpenTrees}>
+      <TreeItem itemType="branch" value="default-subtree-1">
+        <TreeItemLayout>level 1, item 1</TreeItemLayout>
+        <Tree>
+          <TreeItem itemType="leaf">
+            <TreeItemLayout content={{ className: "test-class" }}>level 2, item 1</TreeItemLayout>
+          </TreeItem>
+          <TreeItem itemType="leaf">
+            <TreeItemLayout>level 2, item 2</TreeItemLayout>
+          </TreeItem>
+          <TreeItem itemType="leaf">
+            <TreeItemLayout>level 2, item 3</TreeItemLayout>
+          </TreeItem>
+        </Tree>
+      </TreeItem>
+      <TreeItem itemType="branch" value="default-subtree-2">
+        <TreeItemLayout>level 1, item 2</TreeItemLayout>
+        <Tree>
+          <TreeItem itemType="branch" value="default-subtree-2-1">
+            <TreeItemLayout>level 2, item 1</TreeItemLayout>
+            <Tree>
+              <TreeItem itemType="leaf">
+                <TreeItemLayout>level 3, item 1</TreeItemLayout>
+              </TreeItem>
+              <TreeItem itemType="leaf">
+                <TreeItemLayout>level 3, item 2</TreeItemLayout>
+              </TreeItem>
+              <TreeItem itemType="leaf">
+                <TreeItemLayout>level 3, item 3</TreeItemLayout>
+              </TreeItem>
+            </Tree>
+          </TreeItem>
+        </Tree>
+      </TreeItem>
+    </Tree>
+  );
+};
